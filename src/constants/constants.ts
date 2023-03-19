34159cbf5c9027d49c8b2de51381018d196ada20
אופיר צוקרמן <ofirzukerman@wpyrs-MBP.lan>
2023-03-19 16:23:37 +0200

    added constants

---
+++ src/constants/constants.ts
@@ -0,0 +1,27 @@
+import { Dimensions } from 'react-native';
+
+// colors
+export const colors = {
+    red: '#e32f45',
+    white: '#fff',
+    black: '#000',
+    gray: '#748c94',
+    mainColorBlack: '#2C5364',
+} satisfies Record<string, string>;
+
+
+// shadow
+export const SHADOW = {
+    shadowOffset: {
+        width: 0,
+        height: 10,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 7.0,
+    shadowColor: colors.black,
+    elevation: 24,
+};
+
+// screen
+export const WIDTH_SCREEN: number = Dimensions.get('window').width;
+export const HEIGHT_SCREEN: number = Dimensions.get('window').height;
